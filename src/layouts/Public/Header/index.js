b1f992ad41e6c36405be53cb06133d0d5b2c43c9
Naim Lasker <naim-lasker>
2020-12-31 20:49:01 +0600

    project initialized

--- src/layouts/Public/Header/index.js
+++ src/layouts/Public/Header/index.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
- const Header = () => {
+const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className='header-area'>
+            <div className='container'>
+                <div className='header-top d-flex'>
+                    <div className='header-top-left'>
+                        <p>Welcome to E-commerce</p>
+                    </div>
+                    <div className='header-top-right ml-auto'>
+                        <ul className='d-flex'>
+                            <li>
+                                <a href='/'>About</a>
+                            </li>
+                            <li>
+                                <a href='/'>FAQ</a>
+                            </li>
+                            <li>
+                                <a href='/'>Contact</a>
+                            </li>
+                            <li>
+                                <a href='/'>Wishlist</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
